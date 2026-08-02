447a3e5db0482898ce5e2b86e61d2cbf360c1706
DhroovKanwar <sahildhruv1@gmail.com>
2026-08-02 13:23:18 +0530

    chatbot integrated

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -4,6 +4,7 @@ import Navbar from "../components/sections/Navbar";
 import Footer from "../components/sections/Footer";
 import CartDrawer from "../components/sections/CartDrawer";
 import FloatingOrderCTA from "../components/FloatingOrderCTA";
+import ChatWidget from '../components/chat/ChatWidget'
 
 /**
  * Global chrome: smooth scroll + announcement + navbar + cart drawer + footer.
@@ -18,6 +19,7 @@ export const MainLayout = ({ children }) => {
       <Footer />
       <CartDrawer />
       <FloatingOrderCTA />
+      <ChatWidget />
     </div>
   );
 };
